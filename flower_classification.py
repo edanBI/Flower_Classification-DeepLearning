455bb45489e3f31295677068b926f57b66b88c2d
Eran Edri <eranedrimail@gmail.com>
2019-04-02 17:43:43 +0300

    first push erann

--- flower_classification.py
+++ flower_classification.py
@@ -34,10 +34,8 @@ def plt_modle(model_hist):
 
     plt.show()
 
-# Split images into Training and Validation Sets (20%)
 
 train = ImageDataGenerator(rescale=1./255,horizontal_flip=True, shear_range=0.2, zoom_range=0.2,width_shift_range=0.2,height_shift_range=0.2, fill_mode='nearest', validation_split=0.2)
-
 img_size = 128
 batch_size = 20
 t_steps = 3462/batch_size
@@ -47,37 +45,18 @@ flower_path = "Users/eranedri/Documents/GitHub/Flower_Classification-DeepLearnin
 train_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode='categorical', subset='training')
 valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode = 'categorical', subset='validation')
 
-# Model
 
 model = models.Sequential()
-
-# use model.add() to add any layers you like
-# read Keras documentation to find which layers you can use:
-#           https://keras.io/layers/core/
-#           https://keras.io/layers/convolutional/
-#           https://keras.io/layers/pooling/
-#
-
-# last layer should be with softmax activation function - do not change!!!
 model.add(layers.Flatten())
 model.add(layers.Dense(classes,activation='relu'))
 model.add(layers.Dense(classes,activation='relu'))
 model.add(layers.Dense(classes, activation='softmax'))
-
-# fill optimizer argument using one of keras.optimizers.
-# read Keras documentation : https://keras.io/models/model/
 optimizer =/*keras.optimizers*/
-
-# fill loss argument using keras.losses.
-# reads Keras documentation https://keras.io/losses/
 loss =/*keras.losses*/
 model.compile(loss= loss ,optimizer=optimizer ,metrics=['accuracy'])
-
-# you can change number of epochs by changing the value of the 'epochs' paramter
 model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs= 8 , validation_data=valid_gen, validation_steps=v_steps)
 model.save('flowers_model.h5')
 plt_modle(model_hist)
-
 
 
 def NetworkLoad(PathForModel):
