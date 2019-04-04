86f831948f6e3ae0422e5a855abaa2709dc60949
Eran Edri <eranedrimail@gmail.com>
2019-04-04 16:41:10 +0300

    gui uppdate

--- flower_classification.py
+++ flower_classification.py
@@ -5,77 +5,85 @@ from keras import optimizers
 from keras import losses
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
-from tkinter import *
+import tkinter as tk
+
+
 from tkinter import filedialog
-from PIL import Image
+# from PIL import Image
+#
 
 global model
 
-
+# system interface
 class ModelGUI:
 
-    global DataSetPath
-    global ModelPath
+    # global DataSetPath
+    # global ModelPath
 
-    def __init__(self,master):
-
-
+    def __init__(self, master):
         self.master = master
+        self.DataSetPath = ""
+        self.ModelPath = ""
+
         master.title("Welcome to the best image classification system!")
 
-        self.Header = Label(master, text="Let's Start!").grid(row=0,column=1)
+        self.Header = tk.Label(master, text="Let's Start!").grid(row=0, column=1)
 
-        self.browse_button = Button(master, text="Browse..",width=10,height=2,command=self.Browse,bg="blue").grid(row=1,column=2)
-        self.directions = Label(master,text="Select Dataset File to open:").grid(row=1,column=0)
-        DataSetPath = Entry(master).grid(row=1,column=1)
+        self.browse_button = tk.Button(master, text="Browse..", width=10, height=2, command=self.DatasetLoad()).grid(row=1, column=2)
+        self.directions = tk.Label(master, text="Select Dataset File to open:").grid(row=1, column=0)
+        self.DataSetPath = tk.Entry(master).grid(row=1, column=1)
 
-        self.browse_button = Button(master, text="Browse..",width=10,height=2,command=self.Browse,bg="blue").grid(row=2,column=2)
-        self.directions = Label(master,text="Select Model to load:").grid(row=2,column=0)
-        ModelPath = Entry(master).grid(row=2,column=1)
+        self.browse_button = tk.Button(master, text="Browse..", width=10, height=2, command=self.ModelLoad()).grid(row=2, column=2)
+        self.directions = tk.Label(master, text="Select Model to load:").grid(row=2, column=0)
+        self.ModelPath = tk.Entry(master).grid(row=2, column=1)
 
-        self.predict_button = Button(master, text="Predict", width=25,height=2,command=self.Predict,bg="blue").grid(row=3,column=1)
+        self.predict_button = tk.Button(master, text="Predict", width=25, height=2, command=self.Predict(),
+                                        bg="blue").grid(row=3, column=1)
+        self.restart_button = tk.Button(master, text="Restart", width=25, height=2, command=self.Restart(),
+                                        bg="blue").grid(row=4, column=1)
 
-        self.restart_button = Button(master, text="Restart",width=25,height=2,command=self.Restart,bg="blue").grid(row=4,column=1)
+    # user directory chooser
+    def DatasetLoad(self):
+        if self.DataSetPath is None:
+            self.DataSetPath = filedialog.askdirectory()
 
 
-    def Browse(self):
-        FileName = filedialog.askdirectory()
-        FilePath.set(FileName)
+    # function that load pre saved model
+    def ModelLoad(self):
+        if self.ModelPath is None:
+            self.ModelPath = filedialog.askdirectory()
 
+    # ModelPath = filedialog.askdirectory()
+    # ModelPath.set(ModelName)
+    # model = models.load_model(ModelPath)
+    # predictions = model.predict_generator
+    # print(np.argmax(predictions[0]))
 
-
-    def NetworkLoad(self):
-        ModelPath = filedialog.askdirectory()
-        # ModelPath.set(ModelName)
-        model = models.load_model(ModelPath)
-
+    # first load te model from the modeldir and then classified the chosen dataset from the datasetdir and pop up new window with the results of the model
     def Predict(self):
-        # predictions = model.predict_generator
-        # print(np.argmax(predictions[0]))
+        model = models.load_model(self.ModelPath)
+        model
+
         print("Greetings!")
 
-
+    # restart all the gui fields for new classification
     def Restart(self):
         print("Greetings!")
 
-    def Save(self):
-        print("Greetings!")
 
-
-
-
+# main program that initilized the GUI instane
 class main():
-
-    window = Tk()
+    window = tk.Tk()
     window.geometry("550x200")
     GUI = ModelGUI(window)
     window.mainloop()
 
 
-
+# show results of the trained model
 def plt_modle(model_hist):
     acc = model_hist.history['acc']
     val_acc = model_hist.history['val_acc']
@@ -103,37 +111,36 @@ def plt_modle(model_hist):
 
     plt.show()
 
+
+# training new model on data set
 def TrainModel():
-    train = ImageDataGenerator(rescale=1./255,horizontal_flip=True, shear_range=0.2, zoom_range=0.2,width_shift_range=0.2,height_shift_range=0.2, fill_mode='nearest', validation_split=0.2)
+    train = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True, shear_range=0.2, zoom_range=0.2,
+                               width_shift_range=0.2, height_shift_range=0.2, fill_mode='nearest', validation_split=0.2)
     img_size = 128
     batch_size = 20
-    t_steps = 3462/batch_size
-    v_steps = 861/batch_size
+    t_steps = 3462 / batch_size
+    v_steps = 861 / batch_size
     classes = 5
     flower_path = "/Users/eranedri/Documents/GitHub/Flower_Classification-DeepLearning/flowers"
-    catagories = ["daisy","dandelion","rose","sunflower" ,"tulip"]
-    train_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode='categorical', subset='training')
-    valid_gen = train.flow_from_directory(flower_path, target_size = (img_size, img_size), batch_size = batch_size, class_mode = 'categorical', subset='validation')
-
+    catagories = ["daisy", "dandelion", "rose", "sunflower", "tulip"]
+    train_gen = train.flow_from_directory(flower_path, target_size=(img_size, img_size), batch_size=batch_size,
+                                          class_mode='categorical', subset='training')
+    valid_gen = train.flow_from_directory(flower_path, target_size=(img_size, img_size), batch_size=batch_size,
+                                          class_mode='categorical', subset='validation')
 
     model = models.Sequential()
 
-    model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_size,img_size,3)))
+    model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_size, img_size, 3)))
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
     model.add(layers.Dropout(0.2))
     model.add(layers.Flatten())
-    model.add(layers.Dense(classes,activation='relu'))
+    model.add(layers.Dense(classes, activation='relu'))
     model.add(layers.Dense(classes, activation='softmax'))
 
     optimizer = optimizers.Adam()
     loss = losses.categorical_crossentropy
-    model.compile(loss= loss ,optimizer=optimizer ,metrics=['accuracy'])
-    model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs= 1 , validation_data=valid_gen, validation_steps=v_steps)
+    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+    model_hist = model.fit_generator(train_gen, steps_per_epoch=t_steps, epochs=1, validation_data=valid_gen,
+                                     validation_steps=v_steps)
     model.save('flowers_model.h5')
     plt_modle(model_hist)
-
-
-
-
-
-
